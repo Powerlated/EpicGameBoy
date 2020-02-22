60c64b0b5d08ef6c1555364d39f117a09e68074d
Powerlated <zjia08@gmail.com>
2020-02-21 20:15:53 -0500

    Revert timer implementation

--- src/gameboy/components/timer.ts
+++ src/gameboy/components/timer.ts
@@ -23,33 +23,31 @@ class Timer {
 
     step() {
         // Get the mtime
+        this.c.clock += this.gb.cpu.lastInstructionCycles / 4;
         const BASE = 16;
 
-        for (let i = 0; i < this.gb.cpu.lastInstructionCycles / 4; i++) {
-            this.c.clock += 4;
-            // 1048576hz Divide by 4 = 262144hz
-            if (this.c.clock >= 4) {
-                this.c.mainClock++;
-                this.c.clock -= 4;
+        // 1048576hz Divide by 4 = 262144hz
+        if (this.c.clock >= 4) {
+            this.c.mainClock++;
+            this.c.clock -= 4;
 
-                this.c.divClock++;
-                // Divide by 16 again for 16834hz div clock
-                if (this.c.divClock == 16) {
-                    this.divider++;
-                    this.divider &= 0xFF;
-                    this.c.divClock = 0;
-                }
+            this.c.divClock++;
+            // Divide by 16 again for 16834hz div clock
+            if (this.c.divClock == 16) {
+                this.divider++;
+                this.divider &= 0xFF;
+                this.c.divClock = 0;
+            }
+        }
+
+        if (this.control.running) {
+            if (this.c.mainClock >= Timer.TimerSpeeds[this.control.speed]) {
+                this.counter++;
             }
 
-            if (this.control.running) {
-                if (this.c.mainClock >= Timer.TimerSpeeds[this.control.speed]) {
-                    this.counter++;
-                }
-
-                if (this.counter >= 256) {
-                    this.gb.bus.interrupts.requestTimer();
-                    this.counter = this.modulo;
-                }
+            if (this.counter >= 256) {
+                this.gb.bus.interrupts.requestTimer();
+                this.counter = this.modulo;
             }
         }
     }
