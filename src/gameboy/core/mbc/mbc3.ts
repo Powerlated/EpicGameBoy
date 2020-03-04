52c7141560223c1e451ccec7ad641ac66ef27a6f
Powerlated <zjia08@gmail.com>
2020-03-04 10:01:55 -0500

    Basic RTC in MBC3

--- src/gameboy/core/mbc/mbc3.ts
+++ src/gameboy/core/mbc/mbc3.ts
@@ -24,11 +24,22 @@ export default class MBC3 extends MBCWithRAM implements MBC {
         // RAM Bank 00-03
         if (addr >= 0xA000 && addr <= 0xBFFF) {
             if (this.enableExternalRam) {
-                if (!this.selectRtc) {
+                if (this.ramBank < 0x8) {
                     return this.readBankRam(addr, this.ramBank);
                 } else {
-                    // TODO: Implement RTC
-                    return 0xFF;
+                    let d = new Date();
+                    switch (this.ramBank) {
+                        case 0x08: // Seconds 0-59
+                            return d.getSeconds();
+                        case 0x09: // Minutes 0-59
+                            return d.getMinutes();
+                        case 0x0A: // Hours 0-23
+                            return d.getHours();
+                        case 0x0B: // Days Lower 8 Bits
+                            return 0;
+                        case 0x0C: // Days High Bit, RTC Control 
+                            return 1;
+                    }
                 }
             }
             return 0xFF;
@@ -50,12 +61,12 @@ export default class MBC3 extends MBCWithRAM implements MBC {
             return;
         }
         if (addr >= 0x4000 && addr <= 0x5FFF) {
-
+            this.ramBank = value;
         }
         // RAM Bank 00-0F (Read/Write)
         if (addr >= 0xA000 && addr <= 0xBFFF) {
             if (this.enableExternalRam) {
-                if (!this.selectRtc) {
+                if (this.ramBank < 0x8) {
                     this.writeBankRam(addr, this.ramBank, value);
                 } else {
                     return;
