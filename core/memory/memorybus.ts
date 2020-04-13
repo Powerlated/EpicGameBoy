5bf3a61184e0b1cfa3d4289d7e09b9e13b1fcb78
Powerlated <zjia08@gmail.com>
2020-04-13 03:07:53 -0400

    Use Array<number | null> for cheats instead of Map, it's faster to check

--- core/memory/memorybus.ts
+++ core/memory/memorybus.ts
@@ -55,103 +55,26 @@ class MemoryBus {
 
     serialOut: Array<number> = [];
 
-    cheats: Map<number, number> = new Map();
+    cheats: Array<number | null> = new Array(65536).fill(null);
 
     addCheat(addr: number, value: number) {
-        this.cheats.set(addr, value);
+        this.cheats[addr] = value;
     }
 
-    writeMem8(addr: number, value: number) {
-        // ROM Write (MBC Control)
-        if (addr >= 0x0000 && addr <= 0x7FFF) {
-            this.ext.mbc.write(addr, value);
-        }
+    removeCheat(addr: number) {
+        this.cheats[addr] = null;
+    }
 
-        // Echo RAM
-        if (addr >= 0xE000 && addr <= 0xFDFF) {
-            addr -= 8192;
-        }
-
-        // Write to Internal RAM 
-        if (addr >= 0xC000 && addr <= 0xCFFF) {
-            this.workRamBanks[0][addr - 0xC000] = value;
-        } else if (addr >= 0xD000 && addr <= 0xDFFF) {
-            this.workRamBank[addr - 0xD000] = value;
-        }
-
-        // Write from External RAM through External Bus
-        else if (addr >= 0xA000 && addr <= 0xBFFF) {
-            this.ext.mbc.write(addr, value);
-        }
-
-        // Write to High RAM
-        else if (addr >= 0xFF80 && addr <= 0xFFFE) {
-            this.highRam[addr - 0xFF80] = value;
-        }
-
-        // SET Interrupt request flags
-        else if (addr === INTERRUPT_REQUEST_FLAGS_ADDR) {
-            this.gb.interrupts.requested.setNumerical(value);
-        }
-
-        // SET Interrupt enable flags
-        else if (addr === INTERRUPT_ENABLE_FLAGS_ADDR) {
-            this.gb.interrupts.enabled.setNumerical(value);
-        }
-
-        // Write to VRAM
-        else if (addr >= VRAM_BEGIN && addr <= VRAM_END) {
-            // writeDebug(`[PC 0x${this.cpu.pc.toString(16)}] Wrote to tileset ram 0x${value.toString(16)} @ 0x${addr.toString(16)}`);
-            this.gb.gpu.write(addr, value);
-        }
-
-        // Write to OAM
-        else if (addr >= 0xFE00 && addr <= 0xFE9F) {
-            if (this.gb.gpu.lcdStatus.mode == 0 || this.gb.gpu.lcdStatus.mode == 1) {
-                this.gb.gpu.oam[addr - 0xFE00] = value;
-            }
-            writeDebug(`OAM Write: ${hex(value, 2)} @ ${hex(addr, 4)}`);
-        }
-
-        // Hardware I/O registers
-        else if (addr >= HWIO_BEGIN && addr <= HWIO_END) {
-            this.gb.gpu.writeHwio(addr, value);
-            this.gb.dma.writeHwio(addr, value);
-            this.gb.soundChip.writeHwio(addr, value);
-            this.gb.joypad.writeHwio(addr, value);
-            this.gb.timer.writeHwio(addr, value);
-            switch (addr) {
-                case 0xFF01:
-                    // console.info(`[PC: ${ hex(this.cpu.pc, 4) }, INS: #${ this.cpu.totalI }]SERIAL PORT WRITE: ` + hex(value, 2));
-                    this.serialOut.push(value);
-                    break;
-                case 0xFF4D: // KEY1
-                    if (this.gb.cgb) {
-                        this.gb.prepareSpeedSwitch = (value & 1) === 1;
-                    }
-                    break;
-                case 0xFF50:
-                    if ((value & 1) === 1) {
-                        writeDebug("Disabled bootrom by write to 0xFF50");
-                        this.bootromEnabled = false;
-                    }
-                    break;
-                case 0xFF70:
-                    if (this.gb.cgb) {
-                        if (value === 0) value = 1;
-                        this.workRamBank = this.workRamBanks[value & 0b111];
-                        this.workRamBankIndex = value & 0b111;
-                    }
-                    break;
-                default:
-                    return;
-            }
+    clearCheats() {
+        for (let i = 0; i < this.cheats.length; i++) {
+            this.cheats[i] = null;
         }
     }
 
     readMem8(addr: number): number {
-        if (this.cheats.has(addr)) {
-            return this.cheats.get(addr)!;
+        const cheat = this.cheats[addr];
+        if (cheat !== null) {
+            return cheat;
         }
 
         else if (this.bootromEnabled && addr < 0x100) {
@@ -159,7 +82,7 @@ class MemoryBus {
         }
 
         // Read from ROM through External Bus
-        else if (addr <= 0x7FFF) {
+        else if (addr < 0x8000) {
             return this.ext.mbc.read(addr);
         }
 
@@ -242,6 +165,95 @@ class MemoryBus {
             }
         }
         return 0xFF;
+    }
+
+    writeMem8(addr: number, value: number) {
+        // ROM Write (MBC Control)
+        if (addr < 0x8000) {
+            this.ext.mbc.write(addr, value);
+            return;
+        }
+
+        // Echo RAM
+        if (addr >= 0xE000 && addr <= 0xFDFF) {
+            addr -= 8192;
+        }
+
+        // Write to Internal RAM 
+        if (addr >= 0xC000 && addr <= 0xCFFF) {
+            this.workRamBanks[0][addr - 0xC000] = value;
+        } else if (addr >= 0xD000 && addr <= 0xDFFF) {
+            this.workRamBank[addr - 0xD000] = value;
+        }
+
+        // Write from External RAM through External Bus
+        else if (addr >= 0xA000 && addr <= 0xBFFF) {
+            this.ext.mbc.write(addr, value);
+        }
+
+        // Write to High RAM
+        else if (addr >= 0xFF80 && addr <= 0xFFFE) {
+            this.highRam[addr - 0xFF80] = value;
+        }
+
+        // SET Interrupt request flags
+        else if (addr === INTERRUPT_REQUEST_FLAGS_ADDR) {
+            this.gb.interrupts.requested.setNumerical(value);
+        }
+
+        // SET Interrupt enable flags
+        else if (addr === INTERRUPT_ENABLE_FLAGS_ADDR) {
+            this.gb.interrupts.enabled.setNumerical(value);
+        }
+
+        // Write to VRAM
+        else if (addr >= VRAM_BEGIN && addr <= VRAM_END) {
+            // writeDebug(`[PC 0x${this.cpu.pc.toString(16)}] Wrote to tileset ram 0x${value.toString(16)} @ 0x${addr.toString(16)}`);
+            this.gb.gpu.write(addr, value);
+        }
+
+        // Write to OAM
+        else if (addr >= 0xFE00 && addr <= 0xFE9F) {
+            if (this.gb.gpu.lcdStatus.mode == 0 || this.gb.gpu.lcdStatus.mode == 1) {
+                this.gb.gpu.oam[addr - 0xFE00] = value;
+            }
+            writeDebug(`OAM Write: ${hex(value, 2)} @ ${hex(addr, 4)}`);
+        }
+
+        // Hardware I/O registers
+        else if (addr >= HWIO_BEGIN && addr <= HWIO_END) {
+            this.gb.gpu.writeHwio(addr, value);
+            this.gb.dma.writeHwio(addr, value);
+            this.gb.soundChip.writeHwio(addr, value);
+            this.gb.joypad.writeHwio(addr, value);
+            this.gb.timer.writeHwio(addr, value);
+            switch (addr) {
+                case 0xFF01:
+                    // console.info(`[PC: ${ hex(this.cpu.pc, 4) }, INS: #${ this.cpu.totalI }]SERIAL PORT WRITE: ` + hex(value, 2));
+                    this.serialOut.push(value);
+                    break;
+                case 0xFF4D: // KEY1
+                    if (this.gb.cgb) {
+                        this.gb.prepareSpeedSwitch = (value & 1) === 1;
+                    }
+                    break;
+                case 0xFF50:
+                    if ((value & 1) === 1) {
+                        writeDebug("Disabled bootrom by write to 0xFF50");
+                        this.bootromEnabled = false;
+                    }
+                    break;
+                case 0xFF70:
+                    if (this.gb.cgb) {
+                        if (value === 0) value = 1;
+                        this.workRamBank = this.workRamBanks[value & 0b111];
+                        this.workRamBankIndex = value & 0b111;
+                    }
+                    break;
+                default:
+                    return;
+            }
+        }
     }
 
     readMem16(addr: number) {
