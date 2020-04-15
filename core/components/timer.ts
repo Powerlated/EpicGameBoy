bff4d6fe71c3f6088f0e3fc7e27ebd0c0e3ba95c
Powerlated <zjia08@gmail.com>
2020-04-15 11:43:47 -0400

    Refactor timer HWIO into switch statement

--- core/components/timer.ts
+++ core/components/timer.ts
@@ -2,8 +2,15 @@ import GameBoy from "../gameboy";
 import { hex } from "../../src/gameboy/tools/util";
 import { HWIO } from "../memory/hwio";
 
+
 export default class Timer implements HWIO {
+
     static TimerSpeeds = [1024, 16, 64, 256]; // In terms of 4194304hz division 
+
+
+    constructor(gb: GameBoy) {
+        this.gb = gb;
+    }
 
     gb: GameBoy;
 
@@ -17,39 +24,41 @@ export default class Timer implements HWIO {
     internal = 0;
     mainClock = 0;
 
-
     cyclesBehind = 0;
-
-    constructor(gb: GameBoy) {
-        this.gb = gb;
-    }
 
     readHwio(addr: number): number | null {
         switch (addr) {
             case 0xFF04: // Timer divider
-                return this.addr_0xFF04;
+                return this.internal >> 8;
             case 0xFF05: // Timer counter
-                return this.addr_0xFF05;
+                return this.counter;
             case 0xFF06: // Timer modulo
-                return this.addr_0xFF06;
+                return this.modulo;
             case 0xFF07: // Timer control
-                return this.addr_0xFF07 | 0b11111000;
+                let n = 0;
+
+                n |= (this.control.speed & 0b11); // Bits 0-1
+                if (this.control.running) n |= 0b00000100; // Bit 2
+
+                return n;
         }
         return null;
     }
     writeHwio(addr: number, value: number): void {
         switch (addr) {
             case 0xFF04: // Timer divider
-                this.addr_0xFF04 = value;
+                this.mainClock = 0;
+                this.internal = 0;
                 break;
             case 0xFF05: // Timer counter
-                this.addr_0xFF05 = value;
                 break;
             case 0xFF06: // Timer modulo
-                this.addr_0xFF06 = value;
+                this.modulo = value;
                 break;
             case 0xFF07: // Timer control
-                this.addr_0xFF07 = value;
+                this.control.speed = value & 0b11; // Bits 0-1
+                this.control.running = (value >> 2) !== 0; // Bit 2
+                this.mainClock = 0;
                 break;
         }
     }
@@ -100,53 +109,5 @@ export default class Timer implements HWIO {
         this.internal = 0;
 
         this.cyclesBehind = 0;
-    }
-
-    // Divider
-    get addr_0xFF04(): number {
-        this.catchup();
-        return this.internal >> 8;
-    }
-    set addr_0xFF04(i: number) {
-        // Resets to 0 when written to
-        this.catchup();
-        this.mainClock = 0;
-        this.internal = 0;
-    }
-
-    // Counter / TIMA
-    get addr_0xFF05(): number {
-        this.catchup();
-        return this.counter;
-    }
-    set addr_0xFF05(i: number) {
-        this.catchup();
-    }
-
-    // Modulo
-    get addr_0xFF06(): number {
-        this.catchup();
-        return this.modulo;
-    }
-    set addr_0xFF06(i: number) {
-        this.catchup();
-        this.modulo = i;
-    }
-
-    // Control
-    get addr_0xFF07(): number {
-        this.catchup();
-        let n = 0;
-
-        n |= (this.control.speed & 0b11); // Bits 0-1
-        if (this.control.running) n |= 0b00000100; // Bit 2
-
-        return n;
-    }
-    set addr_0xFF07(i: number) {
-        this.catchup();
-        this.control.speed = i & 0b11; // Bits 0-1
-        this.control.running = (i >> 2) !== 0; // Bit 2
-        this.mainClock = 0;
     }
 }
